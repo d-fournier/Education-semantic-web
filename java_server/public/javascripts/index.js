08a32063f333e7511d8fc495e07c8b121dcceaea
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2014-10-26 10:59:15 +0100

    added jQuery and fixed bug in the formatted JSON

--- java_server/public/javascripts/index.js
+++ java_server/public/javascripts/index.js
@@ -1,45 +1,37 @@
    $(document).ready(function() {
-
        $('#searchForm').submit(function(event) {
            event.preventDefault();
-           var apiKey = "AIzaSyDZjrXVfbGRsUIZpOpB_I9BkIkIhQWoJ_Y";
+           var apiKey2 = "AIzaSyDZjrXVfbGRsUIZpOpB_I9BkIkIhQWoJ_Y";
+           var apiKey = "AIzaSyBOeLl5E9RSrKA0QpWFuF3F91n4rmcPz8o"
            var cx = '016813502462276054558:2encdk-x_ka';
-           var query = $('#query').val();
+           var searchTerms = $('#searchTerms').val();
            //for testing
            var nbResults = 10;
            var resultsPerPage = 10;
            var nbPages = nbResults / resultsPerPage;
+
+           var formattedJSON = {
+
+               //TODO : find a way to get the search engine dynamically
+               searchEngine: "google.fr",
+               results: []
+           };
            for (var i = 0; i < nbPages; i++) {
-               var getQueryResultJsonUrl = "https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + cx + "&q=" + query + "&num=" + resultsPerPage + "&start=" + (i * resultsPerPage + 1);
-
+               var getQueryResultJsonUrl = "https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + cx + "&q=" + searchTerms + "&num=" + resultsPerPage + "&start=" + (i * resultsPerPage + 1);
                $.getJSON(getQueryResultJsonUrl, function(data) {
-                   formatJSON(data);
+                   formattedJSON = formatJSON(data, formattedJSON);
+
                });
-
-               var jqxhr = $.getJSON(getQueryResultJsonUrl, function() {
-                       console.log("success");
-                   })
-                   .done(function() {
-                       console.log("second success");
-                   })
-                   .fail(function() {
-                       console.log("error");
-                   });
            }
-
+           console.log(formattedJSON);
        });
    });
 
-   function formatJSON(rawResponseJSON) {
-       console.log("This is your shit : ");
-       var searchTerms = rawResponseJSON.queries.request[0].searchTerms;
+   function formatJSON(rawResponseJSON, formattedJSON) {
+       if (!formattedJSON.searchTerms)
+           formattedJSON.searchTerms = rawResponseJSON.queries.request[0].searchTerms;
+
        var rawResult = rawResponseJSON.items;
-       var formattedJSON = {
-           search: searchTerms,
-           //TODO : find a way to get the search engine dynamically
-           searchEngine: "google.fr",
-           results: []
-       };
        for (var item in rawResult) {
            var formattedItem = {
                title: rawResult[item].title,
@@ -48,8 +40,9 @@
            };
            formattedJSON.results.push(formattedItem);
        }
-       console.log(formattedJSON);
 
+       return formattedJSON;
 
 
    }
+
