786073d06698150787f4c027b1e0cb5ea5950506
Donovan Fournier <donof43@gmail.com>
2014-11-03 23:10:56 +0100

    Clean Code

--- java_server/app/extraction/InformationExtractor.java
+++ java_server/app/extraction/InformationExtractor.java
@@ -2,15 +2,6 @@ package extraction;
 
 public class InformationExtractor {
 
-	private static final String PATH = "tmp";
-	
-	public static void createInfoFiles(WebSearch request){
-		for(int i = 0; i<request.results.length;i++){
-			String text = request.results[i].title +"\n"+ request.results[i].url + "\n"+request.results[i].description;
-			TextExtractor.saveTextIntoFile(request.searchTerms, request.searchEngine, request.results[i].url, text, ".info");			
-		}
-	}
-	
 	public static WebSearch.WebPagesItem findInfoFromFile(WebSearch request, String filename){
 		for(WebSearch.WebPagesItem item : request.results){
 			String convertedURL = item.url.replaceAll("\"", "_");
