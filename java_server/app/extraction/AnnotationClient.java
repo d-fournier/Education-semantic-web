72d936bed4f49b8a5c4db5531d5333a6cbd03f79
Donovan Fournier <donof43@gmail.com>
2014-11-03 09:57:17 +0100

    Remove Lib

--- java_server/app/extraction/AnnotationClient.java
+++ java_server/app/extraction/AnnotationClient.java
@@ -21,10 +21,6 @@ import org.apache.commons.httpclient.*;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 //import org.apache.commons.logging.Log;
 //import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.Logger;
-import org.dbpedia.spotlight.exceptions.AnnotationException;
-import org.dbpedia.spotlight.model.DBpediaResource;
-import org.dbpedia.spotlight.model.Text;
 
 import java.io.*;
 import java.text.ParseException;
@@ -40,143 +36,158 @@ import java.util.List;
 
 public abstract class AnnotationClient {
 
-    public Logger LOG = Logger.getLogger(this.getClass());
-    
-    // Create an instance of HttpClient.
-    private static HttpClient client = new HttpClient();
+	// Create an instance of HttpClient.
+	private static HttpClient client = new HttpClient();
 
 
-    public String request(HttpMethod method) throws AnnotationException {
+	public String request(HttpMethod method) throws Exception {
 
-        String response = null;
+		String response = null;
 
-        // Provide custom retry handler is necessary
-        method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
-                new DefaultHttpMethodRetryHandler(3, false));
+		// Provide custom retry handler is necessary
+		method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER,
+				new DefaultHttpMethodRetryHandler(3, false));
 
-        try {
-            // Execute the method.
-            int statusCode = client.executeMethod(method);
+		try {
+			// Execute the method.
+			int statusCode = client.executeMethod(method);
 
-            if (statusCode != HttpStatus.SC_OK) {
-                LOG.error("Method failed: " + method.getStatusLine());
-            }
+			if (statusCode != HttpStatus.SC_OK) {
 
-            // Read the response body.
-            byte[] responseBody = method.getResponseBody(); //TODO Going to buffer response body of large or unknown size. Using getResponseBodyAsStream instead is recommended.
+			}
 
-            // Deal with the response.
-            // Use caution: ensure correct character encoding and is not binary data
-            response = new String(responseBody);
+			// Read the response body.
+			byte[] responseBody = method.getResponseBody(); //TODO Going to buffer response body of large or unknown size. Using getResponseBodyAsStream instead is recommended.
 
-        } catch (HttpException e) {
-            LOG.error("Fatal protocol violation: " + e.getMessage());
-            throw new AnnotationException("Protocol error executing HTTP request.",e);
-        } catch (IOException e) {
-            LOG.error("Fatal transport error: " + e.getMessage());
-            LOG.error(method.getQueryString());
-            throw new AnnotationException("Transport error executing HTTP request.",e);
-        } finally {
-            // Release the connection.
-            method.releaseConnection();
-        }
-        return response;
+			// Deal with the response.
+			// Use caution: ensure correct character encoding and is not binary data
+			response = new String(responseBody);
 
-    }
+		} catch (HttpException e) {
+		} catch (IOException e) {
+		} finally {
+			// Release the connection.
+			method.releaseConnection();
+		}
+		return response;
 
-    protected static String readFileAsString(String filePath) throws java.io.IOException{
-        return readFileAsString(new File(filePath));
-    }
-    
-    protected static String readFileAsString(File file) throws IOException {
-        byte[] buffer = new byte[(int) file.length()];
-        BufferedInputStream f = new BufferedInputStream(new FileInputStream(file));
-        f.read(buffer);
-        return new String(buffer);
-    }
+	}
 
-    static abstract class LineParser {
+	protected static String readFileAsString(String filePath) throws java.io.IOException{
+		return readFileAsString(new File(filePath));
+	}
 
-        public abstract String parse(String s) throws ParseException;
+	protected static String readFileAsString(File file) throws IOException {
+		byte[] buffer = new byte[(int) file.length()];
+		BufferedInputStream f = new BufferedInputStream(new FileInputStream(file));
+		f.read(buffer);
+		return new String(buffer);
+	}
 
-        static class ManualDatasetLineParser extends LineParser {
-            public String parse(String s) throws ParseException {
-                return s.trim();
-            }
-        }
+	static abstract class LineParser {
 
-        static class OccTSVLineParser extends LineParser {
-            public String parse(String s) throws ParseException {
-                String result = s;
-                try {
-                    result = s.trim().split("\t")[3];
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    throw new ParseException(e.getMessage(), 3);
-                }
-                return result; 
-            }
-        }
-    }
+		public abstract String parse(String s) throws ParseException;
 
-    public void saveExtractedEntitiesSet(File inputFile, File outputFile, LineParser parser, int restartFrom) throws Exception {
-        PrintWriter out = new PrintWriter(outputFile);
-        LOG.info("Opening input file "+inputFile.getAbsolutePath());
-        String text = readFileAsString(inputFile);
-        int i=0;
-        int correct =0 ;
-        int error = 0;
-       // int sum = 0;
-        for (String snippet: text.split("\n")) {
-            String s = parser.parse(snippet);
-            if (s!= null && !s.equals("")) {
-                i++;
+		static class ManualDatasetLineParser extends LineParser {
+			public String parse(String s) throws ParseException {
+				return s.trim();
+			}
+		}
 
-                if (i<restartFrom) continue;
+		static class OccTSVLineParser extends LineParser {
+			public String parse(String s) throws ParseException {
+				String result = s;
+				try {
+					result = s.trim().split("\t")[3];
+				} catch (ArrayIndexOutOfBoundsException e) {
+					throw new ParseException(e.getMessage(), 3);
+				}
+				return result; 
+			}
+		}
+	}
 
-                List<DBpediaResource> entities = new ArrayList<DBpediaResource>();
-                try {
-                 //   final long startTime = System.nanoTime();
-                    entities = extract(new Text(snippet.replaceAll("\\s+"," ")));
-                 //   final long endTime = System.nanoTime();
-                   // sum += endTime - startTime;
-                   // LOG.info(String.format("(%s) Extraction ran in %s ns.", i, endTime - startTime));
-                    correct++;
-                } catch (AnnotationException e) {
-                    error++;
-                    LOG.error(e);
-                    e.printStackTrace();
-                }
-                for (DBpediaResource e: entities) {
-                    out.println("http://dbpedia.org/resource/"+e.uri());
-                }
-                out.println();
-                out.flush();
-            }
-        }
-        out.close();
-        LOG.info(String.format("Extracted entities from %s text items, with %s successes and %s errors.", i, correct, error));
-        LOG.info("Results saved to: "+outputFile.getAbsolutePath());
-     //   double avg = (new Double(sum) / i);
-      //  LOG.info(String.format("Average extraction time: %s ms", avg * 1000000));
-    }
+	public void saveExtractedEntitiesSet(File inputFile, File outputFile, LineParser parser, int restartFrom) throws Exception {
+		PrintWriter out = new PrintWriter(outputFile);
+		String text = readFileAsString(inputFile);
+		int i=0;
+		int correct =0 ;
+		int error = 0;
+		// int sum = 0;
+//		for (String snippet: text.split("\n")) {
+//			String s = parser.parse(snippet);
+//			if (s!= null && !s.equals("")) {
+//				i++;
+//
+//				if (i<restartFrom) continue;
+//
+//				List<String> entities = new ArrayList<String>();
+//				try {
+//					//   final long startTime = System.nanoTime();
+//					entities = extract(snippet.replaceAll("\\s+"," "));
+//					//   final long endTime = System.nanoTime();
+//					// sum += endTime - startTime;
+//					// LOG.info(String.format("(%s) Extraction ran in %s ns.", i, endTime - startTime));
+//					correct++;
+//				} catch (Exception e) {
+//					error++;
+//					e.printStackTrace();
+//				}
+//				if(entities != null){
+//					for (String e: entities) {
+//						out.println("http://dbpedia.org/resource/"+e);
+//					}					
+//				}
+//				out.println();
+//				out.flush();
+//			}
+//		}
+		String s = parser.parse(text);
+		if (s!= null && !s.equals("")) {
+			i++;
+
+			List<String> entities = new ArrayList<String>();
+			try {
+				//   final long startTime = System.nanoTime();
+				entities = extract(text.replaceAll("\\s+"," "));
+				//   final long endTime = System.nanoTime();
+				// sum += endTime - startTime;
+				// LOG.info(String.format("(%s) Extraction ran in %s ns.", i, endTime - startTime));
+				correct++;
+			} catch (Exception e) {
+				error++;
+				e.printStackTrace();
+			}
+			if(entities != null){
+				for (String e: entities) {
+					out.println("http://dbpedia.org/resource/"+e);
+				}					
+			}
+			out.println();
+			out.flush();
+		}
+		out.close();
+		//   double avg = (new Double(sum) / i);
+		//  LOG.info(String.format("Average extraction time: %s ms", avg * 1000000));
+	}
 
 
-    public void evaluate(File inputFile, File outputFile) throws Exception {
-        evaluateManual(inputFile,outputFile,0);
-    }
+	public void evaluate(File inputFile, File outputFile) throws Exception {
+		evaluateManual(inputFile,outputFile,0);
+	}
 
-    public void evaluateManual(File inputFile, File outputFile, int restartFrom) throws Exception {
-         saveExtractedEntitiesSet(inputFile, outputFile, new LineParser.ManualDatasetLineParser(), restartFrom);
-    }
+	public void evaluateManual(File inputFile, File outputFile, int restartFrom) throws Exception {
+		saveExtractedEntitiesSet(inputFile, outputFile, new LineParser.ManualDatasetLineParser(), restartFrom);
+	}
 
-//    public void evaluateCurcerzan(File inputFile, File outputFile) throws Exception {
-//         saveExtractedEntitiesSet(inputFile, outputFile, new LineParser.OccTSVLineParser());
-//    }
+	//    public void evaluateCurcerzan(File inputFile, File outputFile) throws Exception {
+	//         saveExtractedEntitiesSet(inputFile, outputFile, new LineParser.OccTSVLineParser());
+	//    }
 
-    /**
-     * Entity extraction code.
-     * @param text
-     * @return
-     */
-    public abstract List<DBpediaResource> extract(Text text) throws AnnotationException;
+	/**
+	 * Entity extraction code.
+	 * @param text
+	 * @return
+	 */
+	public abstract List<String> extract(String text);
 }
--- java_server/app/extraction/DBpediaSpotlightClient.java
+++ java_server/app/extraction/DBpediaSpotlightClient.java
@@ -9,9 +9,6 @@ import java.util.List;
 
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.methods.GetMethod;
-import org.dbpedia.spotlight.exceptions.AnnotationException;
-import org.dbpedia.spotlight.model.DBpediaResource;
-import org.dbpedia.spotlight.model.Text;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,100 +16,100 @@ import org.json.JSONObject;
  * Web service-based annotation client for DBpedia Spotlight.
  */
 public class DBpediaSpotlightClient extends AnnotationClient {
-        //private final static String API_URL = "http://jodaiber.dyndns.org:2222/";
-    private final static String API_URL = "http://spotlight.dbpedia.org/";
-        private static final double CONFIDENCE = 0.0;
-        private static final int SUPPORT = 0;
-        @Override
-        public List<DBpediaResource> extract(Text text) throws AnnotationException {
-                String spotlightResponse;
-                try {
-                        GetMethod getMethod = new GetMethod(API_URL + "rest/annotate/?" +
-                                        "confidence=" + CONFIDENCE
-                                        + "&support=" + SUPPORT
-                                        + "&text=" + URLEncoder.encode(text.text(), "utf-8"));
-                        getMethod.addRequestHeader(new Header("Accept", "application/json"));
-                        spotlightResponse = request(getMethod);
-                } catch (UnsupportedEncodingException e) {
-                        throw new AnnotationException("Could not encode text.", e);
-                }
-                assert spotlightResponse != null;
-                JSONObject resultJSON = null;
-                JSONArray entities = null;
-                try {
-                        resultJSON = new JSONObject(spotlightResponse);
-                        entities = resultJSON.getJSONArray("Resources");
-                } catch (JSONException e) {
-                        throw new AnnotationException("Received invalid response from DBpedia Spotlight API.");
-                }
-                LinkedList<DBpediaResource> resources = new LinkedList<DBpediaResource>();
-                for(int i = 0; i < entities.length(); i++) {
-                        try {
-                                JSONObject entity = entities.getJSONObject(i);
-                                resources.add(
-                                                new DBpediaResource(entity.getString("@URI"),
-                                                                Integer.parseInt(entity.getString("@support"))));
-                        } catch (JSONException e) {
-                LOG.error("JSON exception "+e);
-            }
-                }
-                return resources;
-        }
-        
-        public void writeTextConcepts()
-        {
-        	File input = new File(""); //Fichier non annote
-            File output = new File(""); //Fichier annote
-            
-            File folder = new File("tmp");
-            File[] queries = folder.listFiles();
-            
-            for( int i=0; i<queries.length; i++)
-            {
-            	if(queries[i].isDirectory())
-            	{
-            		File[] searchEngines=queries[i].listFiles();
-            		for(int j=0; j< searchEngines.length; j++)
-            		{
-            			if(searchEngines[j].isDirectory())
-            			{
-            				File[] filesToParse=searchEngines[j].listFiles(new FilenameFilter() {									
-								@Override
-								public boolean accept(File dir, String name) {
-									return name.endsWith(".resultsearch");
-								}
-							});
-            				 for(int k=0; k<filesToParse.length; k++)
-            	                {
-            	                        input = filesToParse[k];
-            	                        output = new File(filesToParse[k].getPath().replace(".resultsearch",".dbpedia"));
-            	                        try {
-											evaluate(input, output);
-										} catch (Exception e) {
-											e.printStackTrace();
-										}
-            	                }
-            			}
-            		}
-            		
-            		
-            	}
-            } 
-        }
-        
-        public static void main(String[] args) throws Exception {
-        DBpediaSpotlightClient sc = new DBpediaSpotlightClient ();
-        
-        //Research of concepts linked to Berlin
-        DBpediaLookupClient lc= new DBpediaLookupClient("berlin"); 
-        
-        //Writing of concepts linked to the results of search motors queries
-         sc.writeTextConcepts();
-         
-         //Writing of concepts linked to Berlin (found just before)
-         lc.writeConceptFromQuery("berlin");
-        
-                
-        
-    }
+	//private final static String API_URL = "http://jodaiber.dyndns.org:2222/";
+	private final static String API_URL = "http://spotlight.dbpedia.org/";
+	private static final double CONFIDENCE = 0.2;
+	private static final int SUPPORT = 20;
+
+	@Override
+	public List<String> extract(String text) {
+		String spotlightResponse;
+		try {
+			System.out.println("Launch Spotlight WS");
+			GetMethod getMethod = new GetMethod(API_URL + "rest/annotate/?" +
+					"confidence=" + CONFIDENCE
+					+ "&support=" + SUPPORT
+					+ "&text=" + URLEncoder.encode(text, "utf-8"));
+			getMethod.addRequestHeader(new Header("Accept", "application/json"));
+			spotlightResponse = request(getMethod);
+			System.out.println("Finish Spotlight WS");
+		} catch (Exception e) {
+			return null;
+		}
+		assert spotlightResponse != null;
+		JSONObject resultJSON = null;
+		JSONArray entities = null;
+		try {
+			resultJSON = new JSONObject(spotlightResponse);
+			entities = resultJSON.getJSONArray("Resources");
+		} catch (JSONException e) {
+			return null;
+		}
+		LinkedList<String> resources = new LinkedList<String>();
+		for(int i = 0; i < entities.length(); i++) {
+			try {
+				JSONObject entity = entities.getJSONObject(i);
+				resources.add(new String(entity.getString("@URI")));
+			} catch (JSONException e) {
+			}
+		}
+		return resources;
+	}
+
+	public void writeTextConcepts()
+	{
+		File input = new File(""); //Fichier non annote
+		File output = new File(""); //Fichier annote
+
+		File folder = new File("tmp");
+		File[] queries = folder.listFiles();
+
+		for( int i=0; i<queries.length; i++)
+		{
+			if(queries[i].isDirectory())
+			{
+				File[] searchEngines=queries[i].listFiles();
+				for(int j=0; j< searchEngines.length; j++)
+				{
+					if(searchEngines[j].isDirectory())
+					{
+						File[] filesToParse=searchEngines[j].listFiles(new FilenameFilter() {									
+							@Override
+							public boolean accept(File dir, String name) {
+								return name.endsWith(".resultsearch");
+							}
+						});
+						for(int k=0; k<filesToParse.length; k++)
+						{
+							input = filesToParse[k];
+							output = new File(filesToParse[k].getPath().replace(".resultsearch",".dbpedia"));
+							try {
+								evaluate(input, output);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+
+
+			}
+		} 
+	}
+
+	public static void main(String[] args) throws Exception {
+		DBpediaSpotlightClient sc = new DBpediaSpotlightClient ();
+
+		//Research of concepts linked to Berlin
+		DBpediaLookupClient lc= new DBpediaLookupClient("berlin"); 
+
+		//Writing of concepts linked to the results of search motors queries
+		sc.writeTextConcepts();
+
+		//Writing of concepts linked to Berlin (found just before)
+		lc.writeConceptFromQuery("berlin");
+
+
+
+	}
 }
