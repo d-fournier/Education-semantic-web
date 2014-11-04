17c15a22fd9b066e140fba3904540d9148f983ec
SegoleneM <segolene.minjard@insa-lyon.fr>
2014-11-04 16:45:03 +0100

    Workiiiiiiiiiiiiing ! :dancers:

--- java_server/app/extraction/GraphSimilarity.java
+++ java_server/app/extraction/GraphSimilarity.java
@@ -1,68 +1,58 @@
 package extraction;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FilenameFilter;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-
-import org.apache.jena.riot.RiotException;
 
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.util.FileManager;
+import jaccard.RankingWithJaccard;
+
 
 public class GraphSimilarity {
 
-	public static Map<Long,String> sortGraphs()
+	public static Map<String, Double> sortGraphs(String request)
 	{
-		Map<Long,String> map=new HashMap<Long, String>();
-		File folder = new File("tmp");
-		File[] queries = folder.listFiles();
+		Map<String, Double> map=new HashMap<String, Double>();
+		File folder = new File("tmp/"+request);
 
-		for( int i=0; i<queries.length; i++)
+		File[] queryFileToParse=folder.listFiles(new FilenameFilter() {									
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".rdf");
+			}
+		});
+		Model queryModel=null;
+		for(int l=0; l<queryFileToParse.length; l++)
 		{
-			if(queries[i].isDirectory())
+			queryModel=createGraph(queryFileToParse[l].getPath());
+
+		}
+
+		File[] searchEngines=folder.listFiles();
+
+		for(int j=0; j< searchEngines.length; j++)
+		{
+			if(searchEngines[j].isDirectory())
 			{
-				File[] queryFileToParse=queries[i].listFiles(new FilenameFilter() {									
+				File[] filesToParse=searchEngines[j].listFiles(new FilenameFilter() {									
 					@Override
 					public boolean accept(File dir, String name) {
 						return name.endsWith(".rdf");
 					}
 				});
-				Model queryModel=null;
-				for(int l=0; l<queryFileToParse.length; l++)
+				for(int k=0; k<filesToParse.length; k++)
 				{
-					queryModel=createGraph(queryFileToParse[l].getPath());
-
-				}
-
-				File[] searchEngines=queries[i].listFiles();
-
-				for(int j=0; j< searchEngines.length; j++)
-				{
-					if(searchEngines[j].isDirectory())
-					{
-						File[] filesToParse=searchEngines[j].listFiles(new FilenameFilter() {									
-							@Override
-							public boolean accept(File dir, String name) {
-								return name.endsWith(".rdf");
-							}
-						});
-						for(int k=0; k<filesToParse.length; k++)
-						{
-							Model websiteModel=createGraph(filesToParse[k].getPath());
-							long rank=compareModels(queryModel, websiteModel);
-							map.put(rank, filesToParse[k].getName().replace(".rdf",""));
-						}
-					}
+					Model websiteModel=createGraph(filesToParse[k].getPath());
+					double rank=compareModels(queryModel, websiteModel);
+					map.put( filesToParse[k].getName().replace(".rdf",""), rank);
 				}
 			}
 		}
-
+		RankingWithJaccard.sortMapByValues(map);
 		return map;	
 	}
 
@@ -78,31 +68,18 @@ public class GraphSimilarity {
 					"Fichier: " + inputFileName + " non trouvé");
 		}
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(in));
-		// lire le fichier RDF
-		boolean finished = false;
-		while(!finished){
-			try{
-				model.read(br,null,"N-TRIPLE");
-				finished = true;
-			} catch(RiotException e)
-			{
-				return null;
-			}		
-		}
+		model.read(in,null,"N-TRIPLE");
 
 		return model;
-		// l'écrire sur la sortie standard
-		//		model.write(System.out, "N-TRIPLE");
 	}
-	private static long compareModels(Model m1, Model m2)
+	private static double compareModels(Model m1, Model m2)
 	{
-		return m2.intersection(m1).size()/m2.union(m1).size();
+		return (double)m2.intersection(m1).size()/m2.union(m1).size();
 	}
 
 	public static void main(String[] args)
 	{
-		DBpediaSparqlClient.writeAllRdfFiles("berlin");
-		GraphSimilarity.sortGraphs();
+		DBpediaSparqlClient.writeAllRdfFiles("machin");
+		GraphSimilarity.sortGraphs("machin");
 	}
 }
